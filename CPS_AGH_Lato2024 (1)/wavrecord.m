43f11cf287b07d447d924b139e40aee9c65a32c3
At1z <Adrianlistkiewiczv2@gmail.com>
2024-06-10 14:30:25 +0200

    update

--- CPS_AGH_Lato2024 (1)/wavrecord.m
+++
@@ -1,6 +0,0 @@
-function x = wavrecord( Nx, fs, chan)
-% wavrecord (OLD) via audiorecorder (NEW)
-
-recorder = audiorecorder(fs,8,chan);  % create the recorder
-recordblocking( recorder, Nx/fs );    % record Nx/fs seconds of data
-x = getaudiodata( recorder );         % get the samples   
--- lab11/sortuj.m
+++
@@ -1,6 +0,0 @@
-function [symb, prawd] = sortuj( x );
-symb = unique( x );
-prawd = zeros( size(symb) );
-for n=1:length(prawd)
-    prawd(n)=sum( x == symb(n) );
-end
